2962e8df460fcdb029f4de1bd660a9292d6cbeb3
interu <interu@sonicgarden.jp>
2020-05-01 15:49:29 +0900

    リファクタ

--- lib/cosuka_fargate.rb
+++ lib/cosuka_fargate.rb
@@ -1,11 +1,5 @@
 require "cosuka_fargate/version"
 
-if defined?(::Rails::Railtie)
-  require "cosuka_opsworks/railtie"
-else
-  puts "Please CosukaOpsworks setup by manual."
-end
-
 module CosukaFargate
   class Error < StandardError; end
   # Your code goes here...
--- lib/generators/cosuka_fargate/cosuka_fargate_generator.rb
+++ lib/generators/cosuka_fargate/cosuka_fargate_generator.rb
@@ -1,6 +1,11 @@
-class CosukaFargateGenerator < Rails::Generators::Base
+class CosukaFargateGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   desc 'For fargate setting files'
+
+  def set_app_name
+    # Note: RDS の postgresql では db 名にハイフンが入力できないため
+    @app_name = name.underscore
+  end
 
   def fargate_settings
     template 'Procfile'
